e6479d45bd967c599727991b17d17fca8761fbd7
eunikitin <eu.nikitin@gmail.com>
2017-05-02 01:14:08 +0300

    Add production build mode.

---
+++ webpack.config.build.production.js
@@ -0,0 +1,24 @@
+import path         from 'path';
+import merge        from 'webpack-merge';
+
+import UglifyJsPlugin from 'webpack-uglify-js-plugin';
+
+import baseConfig   from './webpack.config.build.base';
+
+export default merge(
+    baseConfig,
+    {
+        output: {
+            filename: "iterator.min.js",
+        },
+
+        plugins: [new UglifyJsPlugin({
+            sourceMap: false,
+            output: {
+                comments: false
+            },
+            cacheFolder: path.resolve(__dirname, '.tmp/'),
+            minimize: true }
+        )]
+    },
+);
