dea8e90298f82784ac3eb83b6174a1501ea046b4
eu.nikitin <eu.nikitin@gmail.com>
2018-04-19 15:47:29 +0300

    Add react and react-dom package to externals in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV;
 
 const config = {
   input: 'src/index.js',
-  external: [],
+  external: ['react', 'react-dom'],
   output: {
     format: 'umd',
     name: 'npmPackageES6Boilerplate',
