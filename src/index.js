91b26f469c72025116b3823a7eaf85e22293ba0b
eu.nikitin <eu.nikitin@gmail.com>
2018-04-20 10:03:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import       thankYou from './examples/modern';
-
 export const modern = thankYou;
-export       jsx      from './examples/jsx';
-export       flow     from './examples/flow.js';
 
 console.log(thankYou());
