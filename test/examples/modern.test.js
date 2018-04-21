9004b329ff0ebcf61a1cb64019d01829d7c65fcf
eu.nikitin <eu.nikitin@gmail.com>
2018-04-21 23:29:27 +0300

    Add link to package message

--- test/examples/modern.test.js
+++ test/examples/modern.test.js
@@ -7,7 +7,7 @@ const message =
   '  \x1b[42m\x1b[30m                                         \n\x1b[0m' +
   '\n' +
   '  Getting started\n\n' +
-  '  1. Clone the repo from github\n' +
+  '  1. Clone the repo from github (https://github.com/eunikitin/modern-package-boilerplate.git)\n' +
   '  2. Inside the repo directory run npm install && rm -r .git && git init\n' +
   '  3. Update package.json with your information' +
   '\n';
