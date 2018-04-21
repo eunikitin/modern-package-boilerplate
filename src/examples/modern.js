ba4fd4db9338c4ae1c4ce896dc92187c831faff6
eu.nikitin <eu.nikitin@gmail.com>
2018-04-21 19:20:17 +0300

    Update package message

--- src/examples/modern.js
+++ src/examples/modern.js
@@ -1,2 +1,13 @@
 
-export default () => ('Thank you for using this boilerplate!');
+export default () => (
+  '\n' +
+  '  \x1b[42m\x1b[30m                                         \n\x1b[0m' +
+  '  \x1b[42m\x1b[30m  Thank you for using this boilerplate!  \n\x1b[0m' +
+  '  \x1b[42m\x1b[30m                                         \n\x1b[0m' +
+  '\n' +
+  '  Getting started\n\n' +
+  '  1. Clone the repo from github\n' +
+  '  2. Inside the repo directory run npm install && rm -r .git && git init\n' +
+  '  3. Update package.json with your information' +
+  '\n'
+);
