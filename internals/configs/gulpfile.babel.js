cfb02413abd7f6e4a20c4053a67326d60adeb990
eu.nikitin <eu.nikitin@gmail.com>
2018-04-22 19:06:57 +0300

    Add build:prod gulp task

--- internals/configs/gulpfile.babel.js
+++ internals/configs/gulpfile.babel.js
@@ -7,3 +7,5 @@ import cRollup from './rollup.config';
 
 
 gulp.task('build:dev', () => rollup.rollup(cRollup.development).then(bundle => (bundle.write(cRollup.development.output))));
+
+gulp.task('build:prod', () => rollup.rollup(cRollup.production).then(bundle => (bundle.write(cRollup.production.output))));
