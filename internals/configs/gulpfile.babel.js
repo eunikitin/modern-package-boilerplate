f1e07a98936baa3e65b773999c0405edc61344cb
eu.nikitin <eu.nikitin@gmail.com>
2018-04-22 19:07:52 +0300

    Add clean gulp task

--- internals/configs/gulpfile.babel.js
+++ internals/configs/gulpfile.babel.js
@@ -1,10 +1,15 @@
 // Dependencies
-import  gulp from 'gulp';
+import  gulp       from 'gulp';
 import * as rollup from 'rollup';
+import rimraf      from 'rimraf';
+import path        from 'path';
+import fs          from 'fs';
 
 // Configs
-import cRollup from './rollup.config';
+import cRollup     from './rollup.config';
 
+
+gulp.task('clean', (cb) => { rimraf(path.resolve('./lib'), fs, () => {}); cb(); });
 
 gulp.task('build:dev', () => rollup.rollup(cRollup.development).then(bundle => (bundle.write(cRollup.development.output))));
 
