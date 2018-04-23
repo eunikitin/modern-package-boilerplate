4002912ce03d2c56c7a308c8c8c6bac2e5485915
eu.nikitin <eu.nikitin@gmail.com>
2018-04-23 19:18:59 +0300

    Add test and cover gulp tasks

--- internals/configs/gulpfile.babel.js
+++ internals/configs/gulpfile.babel.js
@@ -7,6 +7,7 @@ import fs          from 'fs';
 
 // Configs
 import cRollup     from './rollup.config';
+import cJest       from './jest';
 
 
 gulp.task('clean', (cb) => { rimraf(path.resolve('./lib'), fs, () => {}); cb(); });
@@ -14,3 +15,7 @@ gulp.task('clean', (cb) => { rimraf(path.resolve('./lib'), fs, () => {}); cb(); 
 gulp.task('build:dev', () => rollup.rollup(cRollup.development).then(bundle => (bundle.write(cRollup.development.output))));
 
 gulp.task('build:prod', () => rollup.rollup(cRollup.production).then(bundle => (bundle.write(cRollup.production.output))));
+
+gulp.task('test', () => gulp.src('test').pipe(jest(cJest.test)));
+
+gulp.task('cover', () => gulp.src('test').pipe(jest(cJest.coverage)));
