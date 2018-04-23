e02b8db0819419ceb60af58f71ce7c7808cc4ec8
eu.nikitin <eu.nikitin@gmail.com>
2018-04-23 19:18:20 +0300

    Add jest config

---
+++ internals/configs/jest.js
@@ -0,0 +1,14 @@
+const test = {
+
+};
+
+const coverage = {
+  "collectCoverage": true,
+  "collectCoverageFrom": ["src/**"],
+  "coverageReporters": ["json", "lcov", "text-summary", "html"]
+};
+
+export default {
+  test,
+  coverage
+};
