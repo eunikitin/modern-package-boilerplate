2fc55f882c6e6c0a5b8e8603df8c83e04d3e5996
eunikitin <eu.nikitin@gmail.com>
2019-02-15 20:18:38 +0300

    Add index file for log components

---
+++ internals/builder/core/log/components/index.ts
@@ -0,0 +1,22 @@
+
+
+// Layout
+export * from './append';
+export * from './multiline';
+
+// Styling
+export * from './colorize';
+
+// Common
+export * from './label';
+export * from './date';
+export * from './message';
+export * from './category';
+
+// Debug
+export * from './debugItem';
+
+// Error
+export * from './code';
+export * from './errorType';
+export * from './stack';
