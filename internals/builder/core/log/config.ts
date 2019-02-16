63a707fd2609999f16834642446b7e1b7523aa84
eunikitin <eu.nikitin@gmail.com>
2019-02-16 22:41:04 +0300

    Add logger config

---
+++ internals/builder/core/log/config.ts
@@ -0,0 +1,17 @@
+
+
+export const levels = {
+  fatal:   0,
+  error:   1,
+  warn:    2,
+  info:    3,
+  verbose: 4,
+  debug:   5,
+};
+
+export type ELevels = 'fatal' | 'error' | 'warn' | 'info' | 'verbose' | 'debug';
+
+
+export default {
+  levels,
+};
