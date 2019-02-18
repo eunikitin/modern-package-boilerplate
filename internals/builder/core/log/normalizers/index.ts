dca65758b3ce8106a8c8e981d9fe8426f52e62d3
eunikitin <eu.nikitin@gmail.com>
2019-02-18 05:29:32 +0300

    Add normalizers index file

---
+++ internals/builder/core/log/normalizers/index.ts
@@ -0,0 +1,11 @@
+
+
+export * from './category';
+export * from './code';
+export * from './date';
+export * from './label';
+export * from './message';
+export * from './stack';
+
+export * from './base';
+export * from './warn';
