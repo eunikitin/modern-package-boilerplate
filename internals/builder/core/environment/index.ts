a43fc8ba570fd32e14e1bd1447f169a456c880af
eunikitin <eu.nikitin@gmail.com>
2019-02-22 22:22:09 +0300

    Add environment manager index file

---
+++ internals/builder/core/environment/index.ts
@@ -0,0 +1,8 @@
+// Local
+import Environment                  from './Environment';
+
+
+export default Environment;
+export { default as Environment }   from './Environment';
+export { default as presetLoader }  from './preset-loader';
+export *                            from './types';
