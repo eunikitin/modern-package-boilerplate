5b5174d5c8bb5bfd25d0f81213f9c4305ffa8cf3
eunikitin <eu.nikitin@gmail.com>
2019-02-23 16:06:57 +0300

    Add task runner index file

---
+++ internals/builder/core/task/index.ts
@@ -0,0 +1,10 @@
+// Local
+import TaskRunner from './TaskRunner';
+
+
+export { default as TaskRunner } from  './TaskRunner';
+export *                         from './types';
+
+export default {
+  TaskRunner,
+};
