e2f5a0fbdf4aff755349ed250484fecf7f96761a
eunikitin <eu.nikitin@gmail.com>
2019-02-26 15:22:05 +0300

    Add builder index file

---
+++ internals/builder/core/index.ts
@@ -0,0 +1,10 @@
+// Local
+import Builder from './Builder';
+
+
+export default new Builder();
+export { default as paths } from './paths';
+export *                    from './types';
+
+export { default as log }   from './log';
+export { default as task }  from './task';
