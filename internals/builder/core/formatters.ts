1cc7121495124b64005b6dc67b686e0c22357f1e
eunikitin <eu.nikitin@gmail.com>
2019-02-28 10:23:59 +0300

    Replace moment with dayjs

--- internals/builder/core/formatters.ts
+++ internals/builder/core/formatters.ts
@@ -1,11 +1,11 @@
 // Dependencies
 import * as colors from 'colors';
-import * as moment from 'moment';
+import * as dayjs  from 'dayjs';
 
 
-export const state = (state: string)            => `${state}`;
-export const task  = (task: string)             => `'${colors.cyan(task)}'`;
-export const time  = (time: moment.MomentInput) => `after ${colors.magenta(`${time} ms`)}`;
+export const state = (state: string)           => `${state}`;
+export const task  = (task: string)            => `'${colors.cyan(task)}'`;
+export const time  = (time: dayjs.DayjsObject) => `after ${colors.magenta(`${time} ms`)}`;
 
 export default {
   state,
