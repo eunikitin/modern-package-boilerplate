731e037632d1ff762935bb24d4989c43db033f95
eunikitin <eu.nikitin@gmail.com>
2019-03-02 21:43:39 +0300

    Add test task

---
+++ internals/builder/tasks/test.task.ts
@@ -0,0 +1,34 @@
+// Dependencies
+import * as path   from 'path';
+import { runCLI }  from 'jest-cli';
+
+// Core
+import * as types  from 'builder/core/types';
+import * as paths  from 'builder/core/paths';
+
+
+const test = environment => async function test() {
+  const jestConfig: any = {
+    roots:           ['./package'],
+    testRegex:       '\\.test\\.ts$',
+    collectCoverage: environment.jest.collectCoverage,
+  };
+
+  const success = await runCLI(jestConfig as any, [path.resolve(paths.rootDir())]);
+};
+
+const Task: types.Task = {
+  name: 'test',
+  callback: ({ environment }) => test(environment),
+  cli: ({ runner, cli }) => (
+    cli.command(
+      'test',
+      'test',
+      () => {
+        runner.run('test');
+      },
+    )
+  ),
+};
+
+export default Task;
