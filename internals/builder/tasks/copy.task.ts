b8ed9684f22d1738fbceab1ebe16743210957a27
eunikitin <eu.nikitin@gmail.com>
2019-03-05 14:48:13 +0300

    Update copy task to copy readme and license files

--- internals/builder/tasks/copy.task.ts
+++ internals/builder/tasks/copy.task.ts
@@ -14,18 +14,34 @@ const copy = () => async function copy() {
 
   const packageSrc     = path.resolve(paths.packageDir(), 'package.json');
   const packageLockSrc = path.resolve(paths.packageDir(), 'package-lock.json');
+  const readme         = path.resolve(paths.rootDir(), 'README.md');
+  const license        = path.resolve(paths.rootDir(), 'LICENSE');
 
   if (await fs.exists(packageSrc)) {
     await fs.copyFile(
-      path.resolve(paths.packageDir(), 'package.json'),
-      path.resolve(paths.buildDir(),   'package.json'),
+      path.resolve(paths.rootDir(), 'README.md'),
+      path.resolve(paths.buildDir(), 'package.json'),
     );
   }
 
   if (await fs.exists(packageLockSrc)) {
     await fs.copyFile(
-      path.resolve(paths.packageDir(), 'package-lock.json'),
-      path.resolve(paths.buildDir(),   'package-lock.json'),
+      packageLockSrc,
+      path.resolve(paths.buildDir(), 'package-lock.json'),
+    );
+  }
+
+  if (readme) {
+    await fs.copyFile(
+      readme,
+      path.resolve(paths.buildDir(), 'README.md'),
+    );
+  }
+
+  if (license) {
+    await fs.copyFile(
+      license,
+      path.resolve(paths.buildDir(), 'LICENSE'),
     );
   }
 };
