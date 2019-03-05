5ca7017e9366fee565e075f1078e047be58eeb62
eunikitin <eu.nikitin@gmail.com>
2019-03-05 14:22:28 +0300

    Update develop task description

--- internals/builder/tasks/develop.task.ts
+++ internals/builder/tasks/develop.task.ts
@@ -1,20 +1,4 @@
 import * as types    from 'builder/core/types';
-//
-//
-// const Task: types.Task = {
-//   name: 'develop',
-//   callback: ({ runner }) => runner.series('clean', 'configure', 'devserver'),
-//   cli: ({ runner, cli }) => cli.command(
-//     'develop',
-//     'develop',
-//     {},
-//     () => {
-//       runner.run('develop');
-//     },
-//   ),
-// };
-//
-// export default Task;
 
 
 const Task: types.Task = {
@@ -22,7 +6,7 @@ const Task: types.Task = {
   callback: ({ runner }) => runner.series('clean', 'test', 'lint', 'bundle'),
   cli: ({ runner, cli }) => cli.command(
     'develop',
-    'develop',
+    'Start development mode',
     {},
     () => {
       runner.run('develop');
